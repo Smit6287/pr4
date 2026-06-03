940761f67278342058ffb4665fceaa509ebcee0b
Smit Prajapati <smitprajapati078@gmail.com>
2026-06-03 15:31:24 +0530

    Add files via upload

---
+++ pr4/Q6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", j);
+        }
+        
+        for (int space = 1; space <= 2 * (5 - i); space++) {
+            printf("  "); 
+        }
+        
+        for (int j = i; j >= 1; j--) {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
